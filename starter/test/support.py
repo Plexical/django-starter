3b6e4fd0cd8addaeb58cea0a3734b3a1b67fb24b
Jacob Oscarson <jacob@plexical.com>
2012-02-08 13:59:08 +0100

    Added some helpers that's been useful in in-house projects

---
+++ starter/test/support.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from BeautifulSoup import BeautifulSoup
+
+from django.template import loader, Context, Template
+
+run_template = lambda t, **kw: Template(t).render(Context(kw))
+soup_template = lambda t, **kw: BeautifulSoup(run_template(t, **kw))
+
+def render_raw(name, **ctx):
+    return loader.render_to_string(name, SekizaiContext(ctx))
+
+def render_to_soup(name, **ctx):
+    return BeautifulSoup(render_raw(name, **ctx))
+
+def fake_get(path='/', user=None):
+    from django.test.client import RequestFactory
+    from django.contrib.auth.models import AnonymousUser
+
+    req = RequestFactory().get(path)
+    req.user = AnonymousUser() if user is None else user
+    req.current_page = 'dummy'
+
+    return req
