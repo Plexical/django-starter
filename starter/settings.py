2f28c052fb9d9c3901dfa05a39dbe11e71a1517e
Jacob Oscarson <jacob@plexical.com>
2012-03-02 09:31:40 +0100

    Ops! Late-night l10n slip

--- starter/settings.py
+++ starter/settings.py
@@ -30,7 +30,7 @@ DEFAULT_LANGUAGE = 0
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'Europe/Sweden'
+TIME_ZONE = 'Europe/Stockholm'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
