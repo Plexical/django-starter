ba8e226bc52ad4a06acd624faec4b65f03cbf364
Jacob Oscarson <jacob@plexical.com>
2012-03-12 13:27:43 +0100

    Moved top resource and admin sanity checks to sanity check suite

--- starter/test/test_00_sanity.py
+++ starter/test/test_00_sanity.py
@@ -33,3 +33,15 @@ def test_obligatory(settings, obligatory):
 
 def test_installed_apps(app):
     assert __import__(app)
+
+def test_installation_sane(client):
+    res = client.get('/')
+    assert res.status_code == 200
+    assert res.content == 'Starter app up'
+
+def test_admin_sane(client):
+    res = client.get('/admin/')
+    assert res.status_code == 200
+    dom = BeautifulSoup(res.content)
+    assert (dom.findAll('h1', id='site-name')[0].text ==
+            'Django administration')
