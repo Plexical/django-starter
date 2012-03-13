0fcb1b01ea93b1cc5b5e0507223f987c64124e0a
Jacob Oscarson <jacob@plexical.com>
2012-03-13 15:49:32 +0100

    Fixes #1

--- starter/meta.py
+++ starter/meta.py
@@ -7,9 +7,6 @@ import os
 name = 'starter'
 version = '0.1'
 
-if 'DJANGO_SETTINGS_MODULE' not in os.environ:
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'starter.settings'
-
 def path(*segs):
     from django.conf import settings
     return os.path.abspath(os.path.join(settings.PROJECT_DIR, *segs))
