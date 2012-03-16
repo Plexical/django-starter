35f2b9e601e81c7ec452bb9376ca80cbc37f8a50
Jacob Oscarson <jacob@plexical.com>
2012-03-16 12:15:30 +0100

    Fixes #2 (py.test run problem)

--- starter/conftest.py
+++ starter/conftest.py
@@ -2,7 +2,7 @@ import os, sys
 
 if 'DJANGO_SETTINGS_MODULE' not in os.environ:
     from starter import meta
-    sys.path.insert(0, meta.path())
     os.environ['DJANGO_SETTINGS_MODULE'] = 'starter.develop'
+    sys.path.insert(0, meta.path())
 
 from django_pytest.conftest import *
